ed018e42214faad38a9727aa8d79581ae6e73fe7
Saurabh <165586434+Verma12334@users.noreply.github.com>
2024-04-01 00:52:21 +0530

    Add files via upload

---
+++ Maximum_Value_In_BST.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class node {
+    public:
+    int data;
+    node*left;
+    node*right;
+
+    node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+int Minimum_Value_of_BST(node*root){
+
+    node*temp=root;
+
+    while(temp->left!=NULL){
+        temp=temp->left;
+    }
+    return temp->data;
+}
+
+    node*InsertBST(node*root,int d){
+        if(root==NULL){
+            root=new node(d);
+            return root;
+        }
+        if(d>root->data){
+
+            root->right=InsertBST(root->right,d);
+        }
+        else{
+
+            root->left=InsertBST(root->left,d);
+        }
+        return root;
+    }
+    
+void BST(node*&root){
+    int data;
+    cout<<"Enter the data:\n";
+    cin>>data;
+    while(data!=-1){
+        root=InsertBST(root,data);
+    cout<<"Enter the data:\n";
+        cin>>data;
+    }
+}
+
+int Maximum_Value_of_BST(node*root){
+
+    node*temp=root;
+
+    while(temp->right!=NULL){
+        temp=temp->right;
+    }
+    return temp->data;
+}
+
+int main(){
+    node*root=NULL;
+    BST(root);
+   cout<<"Minimum value of BST "<<Maximum_Value_of_BST(root)<<endl;
+}
